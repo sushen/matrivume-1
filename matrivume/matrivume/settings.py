d88ecb2562d2de566eb3dcf63c4f429c0a9e89de
dev-fahim <fahim6668@gmail.com>
2019-03-16 08:57:53 -0400

    Just Proc

--- matrivume/matrivume/settings.py
+++ matrivume/matrivume/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -144,3 +145,6 @@ ALL_APPS = [
 
 for app in ALL_APPS:
     INSTALLED_APPS.append(app)
+
+# from library django_heroku
+django_heroku.settings(locals())
